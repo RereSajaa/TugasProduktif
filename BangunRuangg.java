aaef8770d795fc21b411709ac2864fdf0476bbaf
rwerwe <156769309+RereSajaa@users.noreply.github.com>
2024-01-23 12:59:21 +0700

    Add files via upload

---
+++ BangunRuangg.java
@@ -0,0 +1,18 @@
+
+package bangunruangg;
+
+
+public class BangunRuangg {
+
+         double volume() {
+        System.out.println("Volume Bangun Ruang");
+        return 0;
+    }
+
+    double luas() {
+        System.out.println("Luas Bangun Ruang");
+        return 0;
+    }
+    }
+    
+
